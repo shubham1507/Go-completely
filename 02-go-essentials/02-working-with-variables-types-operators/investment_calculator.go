ee6dd733f55b0e07a64d21bd50c24835a305fa0d
shubham1507 <shubham.joshi1507@gmail.com>
2025-11-05 07:28:10 +0000

    added var and vals

--- 02-go-essentials/02-working-with-variables-types-operators/investment_calculator.go
+++ 02-go-essentials/02-working-with-variables-types-operators/investment_calculator.go
@@ -6,10 +6,21 @@ import (
 )
 
 func main() {
-	investmentAmount := 1000
-	expectedReturnRate := 5.5
-	year := 10
+	var investmentAmount int
+	const inflationRate = 2.0
+	var expectedReturnRate float64
+	var year int
+
+	fmt.Print("investmentAmount:  ")
+	fmt.Scan(&investmentAmount)
+	fmt.Print("expectedReturnRate:  ")
+	fmt.Scan(&expectedReturnRate)
+	fmt.Print("year:  ")
+	fmt.Scan(&year)
 
 	futureValue := float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(year))
+	futureValueAdjustedForInflation := futureValue / math.Pow(1+inflationRate/100, float64(year))
+
+	fmt.Println(futureValueAdjustedForInflation)
 	fmt.Println(futureValue)
 }
